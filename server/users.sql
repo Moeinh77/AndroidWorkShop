3154816ae63dbd69403467526b0d77e951a270f7
moein h <moeinhasani77@gmail.com>
2018-10-10 10:57:11 +0330

    new db

--- server/users.sql
+++ server/users.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 23, 2018 at 03:02 PM
--- Server version: 5.6.40
--- PHP Version: 5.6.30
+-- Generation Time: Oct 10, 2018 at 07:26 AM
+-- Server version: 5.7.23-0ubuntu0.18.04.1
+-- PHP Version: 7.2.10-0ubuntu0.18.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `professo_presentation`
+-- Database: `users`
 --
 
 -- --------------------------------------------------------
@@ -29,19 +27,31 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `username` varchar(30) DEFAULT NULL,
+  `username` varchar(30) NOT NULL,
   `email` varchar(30) DEFAULT NULL,
-  `password` varchar(30) DEFAULT NULL,
-  `name` varchar(30) DEFAULT NULL
+  `password` text NOT NULL,
+  `mobile` varchar(20) DEFAULT NULL,
+  `location` text CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
+  `signing up date` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`username`, `email`, `password`, `name`) VALUES
-('moeinh77', 'moeinhasani77@gmail.com', '827ccb0eea8a706c4c34a16891f84e', 'moein');
-COMMIT;
+INSERT INTO `users` (`username`, `email`, `password`, `mobile`, `location`, `signing up date`) VALUES
+('moeinh77', 'moeinhasani77@gmail.com', '827ccb0eea8a706c4c34a16891f84e', '0', '0', '2018-10-10 06:13:15'),
+('moein', 'moein@moein.com', '202cb962ac59075b964b07152d234b70', '09123445566', 'hamedan', '2018-10-10 06:18:14');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
